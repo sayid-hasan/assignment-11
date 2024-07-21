d52ab2ec64f7178265d590aa676d668510c7d297
mohammad syed hasan <syedhasanmohammad@gmail.com>
2024-07-21 21:38:46 +0600

    added pics and modified url

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../Provider/AuthProvider/AuthProvider";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://assignment-11-blogys-server.vercel.app",
 });
 const useAxiosSecure = () => {
   const navigate = useNavigate();
